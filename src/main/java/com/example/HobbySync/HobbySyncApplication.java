032d79ecda13b47214304b73d69a1e60a28bc6c3
vaidaoctav <octav.vaida@stud.ubbcluj.ro>
2024-03-30 18:51:23 +0200

    Initial Commit

---
+++ src/main/java/com/example/HobbySync/HobbySyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.HobbySync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbySyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbySyncApplication.class, args);
+	}
+
+}
